7fd830067a152b92ef442b62347a6e049793940a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 21:55:03 -0300

    feat: add entities, db charge, some dto classes

---
+++ ms-pedidos/src/main/java/com/github/hpbtw/ms_pedidos/dto/ItemDoPedidoDTO.java
@@ -0,0 +1,28 @@
+package com.github.hpbtw.ms_pedidos.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ItemDoPedidoDTO {
+    private Long id;
+
+    @NotNull(message = "Campo quantidade é requerido")
+    @Positive(message = "Campo quantidade deve ser maior que 0")
+    private Integer quantidade;
+
+    @NotBlank(message = "Campo descrição é requerido")
+    private String descricao;
+
+    @NotNull(message = "Campo preço unitário é requerido")
+    @Positive(message = "Campo preço unitário é requerido")
+    private BigDecimal precoUnitario;
+}
